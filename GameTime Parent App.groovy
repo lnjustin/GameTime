87e2b180423e29d7c6c649b710ff9c4359168d3b
lnjustin <lnjustin@users.noreply.github.com>
2021-11-05 10:46:20 -0400

    Update GameTime Parent App.groovy

--- GameTime Parent App.groovy
+++ GameTime Parent App.groovy
@@ -29,6 +29,7 @@
  *  v1.4.3 - Improved schedule tile display
  *  v1.5.0 - Improved api key input, added event notifications
  *  v1.5.1 - Fixes issue with pregame event notifications when next game cancelled
+ *  v1.5.2 - Fixes issue with updating tile after the last game of the season
  */
 import java.text.SimpleDateFormat
 import groovy.transform.Field
