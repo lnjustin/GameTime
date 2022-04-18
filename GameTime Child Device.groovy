a248d232938d349b0a17fcc86d1aa60376f114e8
lnjustin <lnjustin@users.noreply.github.com>
2022-04-17 21:40:48 -0400

    Update GameTime Child Device.groovy

--- GameTime Child Device.groovy
+++ GameTime Child Device.groovy
@@ -35,6 +35,7 @@
  *  v1.5.5 - Added ability to configure tile text color from parent GameTime device
  *  v1.5.6 - Fixed issue with NFL post season
  *  v1.5.7 - Gracefully handle unauthorized API access
+ *  v1.5.8 - Fixed update interval bug
 **/
 
 metadata
